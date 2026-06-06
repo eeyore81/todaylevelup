6f2cb5847a389653155fde74a8d702852fc63c1d
juheun park <juhen00@gmail.com>
2026-06-07 08:49:54 +0900

    Initial Commit

---
+++ todaylevelup/todaylevelupApp.swift
@@ -0,0 +1,17 @@
+//
+//  todaylevelupApp.swift
+//  todaylevelup
+//
+//  Created by margarine on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct todaylevelupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
